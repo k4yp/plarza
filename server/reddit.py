153fcb298a6b41945f980f016baed236dc2630fe
kp <66307500+k4yp@users.noreply.github.com>
2023-11-23 06:37:17 +0000

    docker compose

--- server/reddit.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-results = []
-
-def reddit(user_name, count):
-
-    url = f'https://reddit.com/user/{user_name}'
-    response = requests.request('GET', url).json()

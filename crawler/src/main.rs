3dfbe2407ddcda4a62c27aa116b058515b858e00
keyp0s <k4ypos@proton.me>
2023-12-05 17:25:07 +1100

    working yt

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -1,5 +1,9 @@
 use reqwest::Client;
 use serde_json::Value;
+use regex::Regex;
+use itertools::Itertools;
+
+const CHROMEDRIVER_URL: &str = "http://localhost:9515";
 
 #[tokio::main]
 async fn main() {
@@ -14,7 +18,7 @@ async fn main() {
         }
     "#;
 
-    let session = match send_request("http://localhost:9515/session", body).await {
+    let session = match request(format!("{CHROMEDRIVER_URL}/session").as_str(), body).await {
         Ok(session) => session,
         Err(err) => err.to_string(),
     };
@@ -22,20 +26,35 @@ async fn main() {
     let json: Value = serde_json::from_str(&session).unwrap();
     let session_id = json["sessionId"].as_str().unwrap();
 
-    let _ = match send_request(&format!("http://localhost:9515/session/{}/url", session_id), r#"{"url": "https://instagram.com/mrbeast"}"#).await {
+    let _ = match request(&format!("{CHROMEDRIVER_URL}/session/{session_id}/url"), r#"{"url": "https://youtube.com/@mrbeast"}"#).await {
         Ok(response) => response,
         Err(err) => err.to_string(),
     };
 
-    let res = match send_request(&format!("http://localhost:9515/session/{}/source", session_id), "").await {
+    let res = match request(&format!("{CHROMEDRIVER_URL}/session/{session_id}/source"), "").await {
         Ok(response) => response,
         Err(err) => err.to_string(),
     };
 
-    println!("{}", res)
+    let urls = parse_urls(&res, vec![Regex::new(r"watch\?v=([a-zA-Z0-9_-]{11})").unwrap(), Regex::new(r"shorts\?v=([a-zA-Z0-9_-]{11})").unwrap()]);
+    println!("{:?}", urls.unwrap())
 }
 
-async fn send_request(url: &str, body: &str) -> Result<String, reqwest::Error> {
+fn parse_urls(res: &str, patterns: Vec<Regex>) -> Option<Vec<&str>>{
+    let video_ids: Vec<&str> = patterns[0]
+        .captures_iter(res)
+        .map(|captures| captures.get(1).unwrap().as_str())
+        .collect();
+
+    if video_ids.is_empty() {
+        None
+    } else {
+        let unique_video_ids: Vec<&str> = video_ids.iter().unique().cloned().collect();
+        Some(unique_video_ids)
+    }
+}
+
+async fn request(url: &str, body: &str) -> Result<String, reqwest::Error> {
     let response = if body != "" {
         Client::new()
             .post(url)

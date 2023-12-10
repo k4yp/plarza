67bb12ecea03debd064f82f75556a188d5707589
kp <k4ypos@proton.me>
2023-12-10 17:17:54 +1100

    async

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -2,11 +2,20 @@ use reqwest::Client;
 use serde_json::Value;
 use regex::Regex;
 use itertools::Itertools;
+use futures::future;
 
 const CHROMEDRIVER_URL: &str = "http://localhost:9515";
 
 #[tokio::main]
 async fn main() {
+    // let body = r#"
+    //     {
+    //         "desiredCapabilities": {
+    //             "browserName": "chrome"
+    //         }
+    //     }
+    // "#;
+
     let body = r#"
         {
             "desiredCapabilities": {
@@ -26,7 +35,45 @@ async fn main() {
     let json: Value = serde_json::from_str(&session).unwrap();
     let session_id = json["sessionId"].as_str().unwrap();
 
-    let _ = match request(&format!("{CHROMEDRIVER_URL}/session/{session_id}/url"), r#"{"url": "https://youtube.com/@mrbeast"}"#).await {
+
+    let session2 = match request(format!("{CHROMEDRIVER_URL}/session").as_str(), body).await {
+        Ok(session) => session,
+        Err(err) => err.to_string(),
+    };
+
+    let json: Value = serde_json::from_str(&session2).unwrap();
+    let session_id2 = json["sessionId"].as_str().unwrap();
+
+    let session3 = match request(format!("{CHROMEDRIVER_URL}/session").as_str(), body).await {
+        Ok(session) => session,
+        Err(err) => err.to_string(),
+    };
+
+    let json: Value = serde_json::from_str(&session3).unwrap();
+    let session_id3 = json["sessionId"].as_str().unwrap();
+
+    let session4 = match request(format!("{CHROMEDRIVER_URL}/session").as_str(), body).await {
+        Ok(session) => session,
+        Err(err) => err.to_string(),
+    };
+
+    let json: Value = serde_json::from_str(&session4).unwrap();
+    let session_id4 = json["sessionId"].as_str().unwrap();
+
+    let futures = vec![
+                        youtube("nickwhite", session_id),
+                        youtube("bawad", session_id2), 
+                        youtube("theprimeagen", session_id3),
+                        youtube("fireship", session_id4)
+                    ];
+
+    let urls = future::join_all(futures).await;
+
+    println!("{:?}", urls);
+}
+
+async fn youtube(username: &str, session_id: &str) -> Option<Vec<String>> {
+    let _ = match request(&format!("{CHROMEDRIVER_URL}/session/{session_id}/url"), format!("{{\"url\": \"https://youtube.com/@{username}\"}}").as_str()).await {
         Ok(response) => response,
         Err(err) => err.to_string(),
     };
@@ -36,20 +83,20 @@ async fn main() {
         Err(err) => err.to_string(),
     };
 
-    let urls = parse_urls(&res, vec![Regex::new(r"watch\?v=([a-zA-Z0-9_-]{11})").unwrap(), Regex::new(r"shorts\?v=([a-zA-Z0-9_-]{11})").unwrap()]);
-    println!("{:?}", urls.unwrap())
+    parse_urls(&res, vec![r"watch\?v=([a-zA-Z0-9_-]{11})".to_string(), r"shorts\?v=([a-zA-Z0-9_-]{11})".to_string()])
 }
 
-fn parse_urls(res: &str, patterns: Vec<Regex>) -> Option<Vec<&str>>{
-    let video_ids: Vec<&str> = patterns[0]
+fn parse_urls(res: &str, patterns: Vec<String>) -> Option<Vec<String>> {
+    let video_ids: Vec<String> = Regex::new(&patterns[0])
+        .unwrap()
         .captures_iter(res)
-        .map(|captures| captures.get(1).unwrap().as_str())
+        .filter_map(|captures| captures.get(1).map(|m| m.as_str().to_string()))
         .collect();
 
     if video_ids.is_empty() {
         None
     } else {
-        let unique_video_ids: Vec<&str> = video_ids.iter().unique().cloned().collect();
+        let unique_video_ids: Vec<String> = video_ids.iter().unique().cloned().collect();
         Some(unique_video_ids)
     }
 }

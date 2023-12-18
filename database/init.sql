84595a33e3f46456422165292f6eb3e12a8c5e39
k4yp <k4ypos@proton.me>
2023-12-19 10:03:47 +1100

    default db user

--- database/init.sql
+++ database/init.sql
@@ -10,22 +10,25 @@ CREATE TABLE "user" (
     "pfp" VARCHAR(256)
 );
 
+CREATE TABLE "feed" (
+    "feed_id" SERIAL PRIMARY KEY NOT NULL,
+    "username" VARCHAR(32) NOT NULL,
+    "bio" VARCHAR(256),
+    "display" VARCHAR(32),
+    "pfp" VARCHAR(256),
+    "date" BIGINT NOT NULL
+);
+
 CREATE TABLE "post" (
     "post_id" SERIAL PRIMARY KEY NOT NULL,
     "url_id" TEXT NOT NULL,
     "user_id" INTEGER REFERENCES "user"("user_id"),
-    "source" TEXT NOT NULL,
+    "feed_id" INTEGER REFERENCES "feed"("feed_id"),
+    "source" VARCHAR(32) NOT NULL,
     "date" BIGINT NOT NULL,
     "caption" TEXT NOT NULL,
-    "media_path" TEXT,
     "media_url" TEXT
 );
 
-CREATE TABLE "feed" (
-    "userid" SERIAL PRIMARY KEY NOT NULL,
-    "username" VARCHAR(32) NOT NULL,
-    "desc" VARCHAR(256),
-    "display" VARCHAR(32),
-    "pfp" VARCHAR(256),
-    "followers" INT DEFAULT 0
-);
+INSERT INTO "user" ("username", "email", "password", "salt", "date")
+VALUES ('test', 'test@plarza.com', '$argon2id$v=19$m=19456,t=2,p=1$gkc15RVOcVHKXDYGvZMcww$d+V7MsxUnGNXzMzcAK/vEhqXKkLJjrmsn/cxWR2sJ3g', 'gkc15RVOcVHKXDYGvZMcww', 1700000000);

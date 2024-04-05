e15157d7f9c1d264be5511130fec4f926d91906e
kp <k4ypos@proton.me>
2024-04-05 00:40:26 +0000

    vercel analytics

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
+    import { inject } from '@vercel/analytics'
     import Navbar from "./Navbar.svelte";
     import Footer from "./Footer.svelte";
+
+    inject({ mode: 'production' });
 </script>
 
 <Navbar />
